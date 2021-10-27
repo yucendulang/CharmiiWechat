7545e587fa43be4f2a2e97f3c24fdffca0e72e16
YuChen <yucenyucen@126.com>
2021-10-27 10:46:07 +0800

    feat:役满榜增加删除功能

--- miniprogram/pages/yukaman/yukamanDisplay/yukamanDisplay.js
+++ miniprogram/pages/yukaman/yukamanDisplay/yukamanDisplay.js
@@ -1,4 +1,5 @@
 // pages/yukaman/yukamanDisplay.js
+const app = getApp()
 Page({
 
   /**
@@ -8,7 +9,8 @@ Page({
     yukamanTypeArray: ["所有", "天和", "地和", "四暗刻", "大三元", "国士无双", "字一色", "绿一色", "小四喜", "清老头", "九莲宝灯", "四杠子", "累计役满", "纯正九莲宝灯", "四暗刻单骑", "国士无双十三面", "大四喜"],
     yukamanTypeValue: 0,
     searchName: "",
-    inputValue:"",
+    inputValue: "",
+    role: app.globalData.role,
   },
 
   /**
@@ -29,6 +31,9 @@ Page({
           console.log(e)
         }
       }
+    })
+    this.setData({
+      role: app.globalData.role
     })
   },
 
@@ -82,20 +87,20 @@ Page({
   },
 
   yukamanTypeBindPickerChange: function (e) {
-    console.log('yukamanPicker发送选择改变，携带值为', e.detail.value,this.data.yukamanTypeValue,this.data.inputValue)
+    console.log('yukamanPicker发送选择改变，携带值为', e.detail.value, this.data.yukamanTypeValue, this.data.inputValue)
     this.setData({
       yukamanTypeValue: e.detail.value
     })
     var filterList
     if (e.detail.value == 0) {
       filterList = this.myYakuman
-    }else{
+    } else {
       filterList = this.myYakuman.filter(item => item.yukamanType == this.data.yukamanTypeArray[e.detail.value])
     }
 
 
-    if (this.data.inputValue!=""&&this.data.inputValue!=null){
-      filterList=filterList.filter(i=>i.playerName.indexOf(this.data.inputValue) != -1)
+    if (this.data.inputValue != "" && this.data.inputValue != null) {
+      filterList = filterList.filter(i => i.playerName.indexOf(this.data.inputValue) != -1)
     }
 
     this.setData({
@@ -104,20 +109,64 @@ Page({
   },
 
   bindKeyInput: function (e) {
-    console.log("[bindKeyInput]",e.detail.value)
+    console.log("[bindKeyInput]", e.detail.value)
     var filterList
     if (this.data.yukamanTypeValue == 0) {
       filterList = this.myYakuman
-    }else{
+    } else {
       filterList = this.myYakuman.filter(item => item.yukamanType == this.data.yukamanTypeArray[this.data.yukamanTypeValue])
     }
 
-    if (e.detail.value!=""&&e.detail.value!=null){
-      filterList=filterList.filter(i=>i.playerName.indexOf(e.detail.value) != -1)
+    if (e.detail.value != "" && e.detail.value != null) {
+      filterList = filterList.filter(i => i.playerName.indexOf(e.detail.value) != -1)
     }
 
     this.setData({
       myYakuman: filterList
     })
   },
+
+  del:function(e){
+    console.log('[del]',e)
+    console.log(this)
+    var that=this
+    var id=e.target.dataset.id
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除吗？',
+      success: function (sm) {
+        if (sm.confirm) {
+          console.log(that)
+          that.cancelYukaman(id)
+          wx.redirectTo({
+            url: "/pages/yukaman/yukamanDisplay/yukamanDisplay"
+        })
+          } else if (sm.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    },
+
+    cancelYukaman: function (id,func) {
+      console.log('[cancelYukaman]取消的id是',id)
+      const db = wx.cloud.database()
+      db.collection('yakuman').doc(id)
+        .remove({
+          success: res => {
+            // 在返回结果中会包含新创建的记录的 _id
+            wx.showToast({
+              title: '取消成功',
+            })
+            func()
+          },
+          fail: err => {
+            wx.showToast({
+              icon: 'none',
+              title: '取消失败'
+            })
+            console.error('[数据库] [新增记录] 失败：', err)
+          }
+        })
+    },
 })
